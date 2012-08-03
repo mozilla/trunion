39188e08cb03fb4dcb7096ac7c21077cd1696f4f
Jeremiah Orem <jeremy.orem@gmail.com>
2012-08-03 13:07:27 -0700

    Add cli options to verify_keys.

--- scripts/verify_keys.py
+++ scripts/verify_keys.py
@@ -1,13 +1,18 @@
 import os
 import sys
-
-
-usage = 'verify_keys.py [cert] [key]'
+from optparse import OptionParser
 
 sys.path.append(os.path.join(
-                 os.path.dirname(os.path.abspath(__file__)), '..'))
+                os.path.dirname(os.path.abspath(__file__)), '..'))
 
 from trunion.utils import check_keys
 
+parser = OptionParser(usage='verify_keys.py [-e expiry] [cert] [key]')
+parser.add_option("-e", "--expires", type='int',
+                  help='check that key is valid until now + expiry')
 
-check_keys(sys.argv[1], sys.argv[2])
+
+(options, args) = parser.parse_args()
+
+
+check_keys(args[0], args[1], check_expiration=options.expires)
